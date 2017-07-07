0e2ce909d2996d61b42333cf848eb5ab618d3cfc
gogabidzia <gogabidzia@gmail.com>
2017-07-08 02:51:16 +0400

    advModal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,22 +130,7 @@
   </div>
 </div>
 
-<div id="advModal" class="modal fade loginModal" role="dialog">
-  <div class="modal-dialog">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h3 class="modal-title text-center">
-        	რეკლამა
-        </h3>
-      </div>
-      <div class="modal-body">
-      		რეკლამის პირობები
-      </div>
-    </div>
-  </div>
-</div>
+@include('partials.advmodal')
 
 <script type="text/javascript" src="/js/jquery.min.js"></script>
 <script type="text/javascript" src="/js/bootstrap.min.js"></script>
---
+++ resources/views/partials/advmodal.blade.php
@@ -0,0 +1,57 @@
+
+<div id="advModal" class="modal fade loginModal" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h3 class="modal-title text-center">
+        	რეკლამა
+        </h3>
+      </div>
+      <div class="modal-body">
+        <h4>
+          1.დიდი ჰორიზონტალური ბანერი
+        </h4>
+        <ul>
+          <li>ზომა პიქსელებში: 1300X5000</li>
+          <li>ფაილის ფორმატი: GIF, JliG, SWF/Flash</li>
+          <li>ღირებულება: 200 ლარი თვეში</li>
+        </ul>
+        <hr>
+        <h4>
+          2.ჰორიზონტალური ბანერი საიტის ქვედა ნაწილში
+        </h4>
+        <ul>
+          <li>ზომა პიქსელებში: 1300X5000</li>
+          <li>ფაილის ფორმატი: GIF, JliG, SWF/Flash</li>
+          <li>ღირებულება: 400 ლარი თვეში</li>
+        </ul>
+        <hr>
+        <h4>
+          3.ბანერი (კვადრატული)
+        </h4>
+        <ul>
+          <li>ფაილის ფორმატი: GIF, JliG, SWF/Flash</li>
+          <li>ღირებულება: 150 ლარი თვეში</li>
+        </ul>
+        <hr>
+        <h4>
+          4. ბუშტის სიახლეებში მოხვედრა
+        </h4>
+        <ul>
+          <li>ღირებულება: 50 ლარი თვეში</li>
+        </ul>
+        <hr>
+        <h4 class="text-center">ანგარიშსწორება</h4>
+        <ul>
+          <li style="font-weight: bold; margin-left: -25px;">თიბისი ბანკში</li>
+          <li>ანგარიშის #: GE51TB7365636080100005 მიმღები: შპს “ისუნთქე ქალაქში“</li>
+          <li>საბანკო კოდია კოდია TBCBGE 22</li>
+          <li style="font-weight: bold; margin-left: -25px">საქართველოს ბანკში</li>
+          <li>ანგარიშის #: GE25BG0000000104066400 მიმღები შპს “ისუნთქე ქალაქში“</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
