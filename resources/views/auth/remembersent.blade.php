72af4e73b89c7e7f31a51faa7e11ed4ceddce1c4
gogabidzia <gogabidzia@gmail.com>
2017-07-09 20:23:02 +0400

    remember

---
+++ resources/views/auth/remembersent.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		პაროლის გახსენება - Vime
+	</title>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+<div class="container" style="margin-top: 150px;">
+	<div class="panel panel-default">
+		<div class="panel-body"><h4>პაროლის გახსენება</h4></div>
+		<div class="panel-footer">
+			<div class="alert alert-success">
+				წერილი წარმატებით გაიგზავნა
+			</div>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript" src="/js/jquery.min.js"></script>
+<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+	
+</script>
+</body>
+</html>
