38ded62777896030f94dd6eb85ea9bfa0daa757c
gogabidzia <gogabidzia@gmail.com>
2017-07-10 09:29:54 +0400

    header banner

--- resources/views/partials/headerbanner.blade.php
+++ resources/views/partials/headerbanner.blade.php
@@ -16,10 +16,18 @@
 		<div class="item">
 			<div class="row">
 				<div style="color:#111; margin-top: 100px;" class="col-md-12 tbcfonted">
-					<h1>ეწვიეთ ჩვენს <a href="https://www.facebook.com/vime.ge/">ფეისბუქ გვერდს.</a></h1>
+					<div class="fb-page" data-href="https://www.facebook.com/vime.ge/" data-tabs="timeline" data-height="337" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/vime.ge/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/vime.ge/">Vime.Ge - ვიმე</a></blockquote></div>
 				</div>
 			</div>
 		</div>
 	</div>
 
 </div>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=397901047213571";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
