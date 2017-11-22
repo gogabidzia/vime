732a41b88144269b45710c1a4c69f56502b0a9fb
DESKTOP-FG843I3\tazom <tazo.mindiashvili1@gmail.com>
2017-11-23 01:11:08 +0400

    ჩვენს შესახებ

--- resources/views/app/about.blade.php
+++ resources/views/app/about.blade.php
@@ -9,7 +9,7 @@
 <div id="content">
 	<div class="container">
 		<h1 class="text-center segoefonted">ჩვენს შესახებ</h1>
-		<p class="text-center">Vime.ge შეიქმენა 2017 წელს.
+		<p class="text-center">Vime.ge შეიქმნა 2017 წელს.
 		ის საქართველოში პირველი ვიდეო ვაკანსიების (ვიზუმეების) საიტია.<br>
 
 		ჩვენი მიზანია მომხმარებლის და დამსაქმებლის მარტივი დაკვაშირება ერთმანეთთან. </br> 
