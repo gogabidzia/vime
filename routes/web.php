0cf8512ffeae23c807ade3eeaf953d1030e2d72c
Giorgi Tsirekidze <goga@zipier.com>
2017-11-26 01:17:08 +0400

    test

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,9 @@ Route::post('/vacancies/add', 'VacancyController@add')->middleware('ifCompany');
 Route::get('/vacancies/remove/{id}', 'VacancyController@remove')->middleware('ifCompany');
 
 Route::get('/vacancies/all/{id}' , 'VacancyController@view');
-Route::post('/vacancies/bid', 'VacancyController@bid');
+Route::post('/vacancies/bid', function(){
+	dd('here');
+});
 
 Route::get('/logos/{image}', 'ProfileController@getImage');
 Route::get('/videos/{name}', 'ProfileController@getVideo');
