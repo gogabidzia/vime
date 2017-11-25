408e52847675b474e1ef114e645756fcad28790b
DESKTOP-FG843I3\tazom <tazo.mindiashvili1@gmail.com>
2017-11-25 22:56:33 +0400

    vertical modal

--- resources/views/partials/advmodal.blade.php
+++ resources/views/partials/advmodal.blade.php
@@ -1,21 +1,14 @@
 
-<div id="advModal" class="modal fade loginModal" role="dialog">
-  <div class="modal-dialog-adv">
+<div id="advModal" class="modal fade loginModalTwo" role="dialog">
+  <div class="modal-dialog">
     <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-header-adv">
+      <div class="modal-header">
 
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h3 class="modal-title text-center">
-        	
+        	<span style="cursor:vertical-text"><h1><div style="color:#ffb322">visume@gmail.com</div></h1></span>
         </h3>
-      </div>
-      <div class="modal-body-adv">
-
-      <h1><div id="myDiv">vimesume@gmail.com</div></h1>
-      
-         
-        
       </div>
     </div>
   </div>
