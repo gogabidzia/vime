7fde59b96a676e08f9e9acedf6ab70695fb38cb0
DESKTOP-FG843I3\tazom <tazo.mindiashvili1@gmail.com>
2017-11-25 23:48:46 +0400

    background:none

--- resources/views/partials/advmodal.blade.php
+++ resources/views/partials/advmodal.blade.php
@@ -2,7 +2,7 @@
 <div id="advModal" class="modal fade loginModalTwo" role="dialog">
   <div class="modal-dialog">
     <!-- Modal content-->
-    <div class="modal-content">
+    <div class="modal-contentAdv">
       <div class="modal-header">
 
         <button type="button" class="close" data-dismiss="modal">&times;</button>
