37d33bf7f40d141a0f3870f11e8235328bd36592
Giorgi Tsirekidze <goga@zipier.com>
2017-11-26 01:27:41 +0400

    test

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -89,8 +89,7 @@ class VacancyController extends Controller
     }
 
     public function bid(Request $request){
-	dd('here');
-exit;
+	exit;
         $video = Video::findOrFail($request->get('video_id'));
         if($video && $request->user()->id !== $video->user->id ){
             return redirect()->back();
