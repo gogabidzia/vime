e4af7df86d209a01441c09eafbf724e9c52dc755
DESKTOP-FG843I3\tazom <tazo.mindiashvili1@gmail.com>
2018-01-27 01:12:58 +0400

    about

--- resources/views/app/about.blade.php
+++ resources/views/app/about.blade.php
@@ -11,14 +11,9 @@
 		<h1 class="text-center segoefonted">ჩვენს შესახებ</h1>
 		<p class="text-center">
 
-			 <p>გვსურს გაცნობოთ ახალი და ეფექტური საშუალების შესახებ თქვენი გუნდის წევრის მოსაძებნად. რისთვისაც შევქმენით ინოვაციური და უანალოგო ვებ-გვერდი საქართველოსთვის სახელად <b>vime.ge,</b> რომლის მეშვეობითაც დამსაქმებელს შეუძლია გამოაქვეყნოს შემოთავაზებები ვაკანსიებთან დაკავშირებით, ხოლო მომავალ თანამშრომელს ელექტრონული რეზიუმეს ნაცვლად აქვს შესაძლებლობა გამოგიგზავნოთ ვიდეო-რეზიუმე. ამ სიახლის წყალობით, თქვენ თანამშრომელს უფრო ოპერატიულად აიყვანთ,
-იმედი გვაქვს ჩვენი საიტი დაგეხმარებათ დროის დაზოგვაში, მადლობა ყურადღებისთვის.<p>
+			 vime.ge ვიდეო ვაკანსიების საიტია, რომელიც დაზოგავს თქვენს დროს.
 
-
-     we want to inform you about a new and an effective way of employing. That’s why we have created a innovation novel web-site called <b>http://vime.ge/,</b> where employer can post an offer and future employee instead of CV can send a video-resume. Thanks to this novelty there will not be any need for job seekers to interview.
-We hope you will find our web-site helpful, thanks in regards.
-
-		<a href="#" data-toggle="modal" data-target="#instructionsModal">ინსტრუქცია</a>.</p>
+		<!-- <a href="#" data-toggle="modal" data-target="#instructionsModal">ინსტრუქცია</a>.</p> -->
 	</div>
 </div>
 @stop
