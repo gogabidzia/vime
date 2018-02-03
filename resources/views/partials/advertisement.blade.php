8f6420c03159902cb2d30f8fa44681bd5fcf8901
DESKTOP-FG843I3\tazom <tazo.mindiashvili1@gmail.com>
2018-02-03 20:35:15 +0400

    .

--- resources/views/partials/advertisement.blade.php
+++ resources/views/partials/advertisement.blade.php
@@ -1,3 +1,3 @@
 <div class="advertisements">
-	<a href="https://www.facebook.com/dudastojinebi/" target='_blank'><img src="{{ asset('img/snapseed.jpg') }}" style="width:200px; height: 415px;"></a>
+	<a href="https://www.facebook.com/dudastojinebi/" target='_blank'><img src="{{asset('img/snapseed.png')}}" style="width:200px; height: 415px;"></a>
 </div>
